356e42bcdae0bd61e534bf8867ba419d39497a8c
BSI80207 <boanfourierr@gmail.com>
2024-03-08 10:45:13 +0700

    Project MVC

---
+++ SampleMVC/Controllers/ArticleController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebFormApp.BLL.Interfaces;
+using MyWebFormApp.BLL.DTOs;
+namespace SampleMVC.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly IArticleBLL _articleBLL;
+        public ArticleController(IArticleBLL articleBLL)
+        {
+            _articleBLL = articleBLL;
+        }
+            public IActionResult Index()
+        {
+            var models = _articleBLL.GetArticleWithCategory();
+            return View(models);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ArticleDTO article)
+        {
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(ArticleDTO article)
+        {
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Search(string search)
+        {
+
+            return View("Index");
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
